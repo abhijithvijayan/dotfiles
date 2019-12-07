055443a942ae1f968d369bcc89be2fc1a7535630
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-07 16:15:39 +0530

    new alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,35 +3,21 @@
 #   (run `alias`)
 #-------------------
 
-# Git Aliases
-alias gcm="git commit -m"
-alias gcam='git commit -a -m'
-alias gca="git commit --amend --no-edit"
-alias gcae="git commit --amend"
-alias gcaa="git add --all && git commit --amend --no-edit"
-alias gcaae="git add --all && git commit --amend"
-alias gap="git add -p"
-alias gnope="git checkout ."
-alias gwait="git reset HEAD"
-alias gundo="git reset --soft HEAD^"
-alias greset="git clean -f && git reset --hard HEAD" # Removes all changes, even untracked files
-alias gl="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias glb='git log --oneline --decorate --graph --all'
-alias gst='git status -s'
-alias gpl="git pull --rebase"
-alias gps="git push"
-alias gpsf="git push --force-with-lease"
-alias grb="git rebase"
-alias grbi='git rebase -i'
-alias grba='git rebase --abort'
-alias grbc='git rebase --continue'
-# Remove local branches that have already been merged in the remote repository
-alias gcmb="git branch --merged | grep -Ev '(^\*|master)' | xargs git branch -d"
-# Sets the upstream branch to be the same branch as the locally named branch
-alias gset='git branch --set-upstream-to=origin/`git symbolic-ref --short HEAD`'
-
-
 # General Aliases
+alias pb='pbcopy'
+alias te='tree'
+alias op='open'
+alias g='git' # Wrapper over git
+alias _='sudo'
+alias du='du -sh * | sort'
+alias eo='echo'
+alias d='cd'
+alias lg='ls | grep'
+alias m='mkdir'
+alias q='exit'
+alias screen='screenfetch'
+alias mx='chmod +x'
+alias rr='rm -rf'
 
 # Open .zshrc to be edited in VS Code
 alias config="code ~/.zshrc"
@@ -43,3 +29,78 @@ alias ls='colorls'
 alias ll='colorls --group-directories-first --almost-all --long'
 # Clear terminal
 alias c='clear'
+
+# Python
+alias p='python3'
+alias p2='python2'
+
+# Docker
+alias c='docker'
+alias cs='docker ps'
+alias cu='docker-compose up'
+
+# Node
+alias no='node'
+
+# Yarn
+alias ja='yarn add'
+alias jt='yarn test'
+alias jr='yarn run'
+alias je='yarn remove'
+alias js='yarn start'
+alias jv='yarn run dev'
+alias jg='yarn global add'
+alias jd='yarn add --dev'
+
+# Nginx
+alias ngup='sudo nginx'
+alias ngdown='sudo nginx -s stop'
+alias ngre='sudo nginx -s stop && sudo nginx'
+alias nglog='tail -f /usr/local/var/log/nginx/access.log'
+alias ngerr='tail -f /usr/local/var/log/nginx/error.log'
+
+alias cpu='top -o cpu'   # CPU
+alias mem='top -o rsize' # Memory
+
+# easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+# cd places
+alias doc='cd ~/Documents/'
+alias dz='cd ~/.dotfiles'
+alias de='cd ~/Desktop'
+alias dq='cd ~/Downloads'
+
+# Utilities
+alias ungit="find . -name '.git' -exec rm -rf {} \;" # Remove git from a project
+
+# Git Aliases
+alias gj='open `git config remote.origin.url`'
+alias gl="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glb='git log --oneline --decorate --graph --all'
+alias gpm="git push origin master"
+alias grl='git reset --hard && git clean -df' # Reset to exact state as last commit
+alias gb='git checkout -b'
+alias gr='git reset --hard'
+alias g:='git push -u origin master'
+alias ggf='git push -f'
+alias gi='git init'
+alias gl='git pull'
+alias gn='git clone'
+alias g.='git add .'
+alias ga='git add'
+alias grao='git remote add origin'
+
+# Undo your last commit, but don't throw away your changes
+alias greset='git reset --soft HEAD^'
+
+# Git Diff
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gdn='git diff --name-only'
+
+# Git Status
+alias gs='git status --short'
+alias gss='git show --word-diff=color' # See changes made
