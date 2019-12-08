e80cbea17a0afe7e4db1bd392085e001d23f98e6
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-08 20:48:03 +0530

    add conda config

--- zsh/others.zsh
+++ zsh/others.zsh
@@ -14,3 +14,19 @@ source /usr/local/share/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
+
+# Anaconda setup
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/${USER}/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/${USER}/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/${USER}/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/${USER}/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,13 +4,18 @@
 
 # Flutter
 export PATH="$PATH:$HOME/flutter/bin"
+
 # Android Studio
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
 # Chrome web driver
 export PATH=$PATH:/usr/local/share/
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
 
 # Path to your oh-my-zsh installation.
 export ZSH="/home/${USER}/.oh-my-zsh"
