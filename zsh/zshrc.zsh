e572b462c9c7b08834f70d21401e899ff5d587c9
Abhijith Vijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-07-19 11:03:42 +0530

    fix: order of import

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,5 +1,6 @@
 source ~/.dotfiles/zsh/env.zsh
 source ~/.dotfiles/zsh/path.zsh
+source ~/.dotfiles/zsh/others.zsh
 source ~/.dotfiles/zsh/hooks.zsh
 source ~/.dotfiles/zsh/options.zsh
 source ~/.dotfiles/zsh/functions/functions.zsh
@@ -7,4 +8,3 @@ source ~/.dotfiles/zsh/functions/git-functions.zsh
 source ~/.dotfiles/zsh/bindings.zsh
 source ~/.dotfiles/zsh/locale.zsh
 source ~/.dotfiles/zsh/alias.zsh
-source ~/.dotfiles/zsh/others.zsh
